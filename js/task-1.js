eff2aa9c09f7a90f79523dfb5185f98b7f411fe5
Manokha Vadim <manokha.vadim@gmail.com>
2025-04-22 23:07:49 +0300

    modified project

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
-const categoriesList = document.querySelectorAll('#categories .item');
-console.log(`Number of categories: ${categoriesList.length}`);
+// const categoriesList = document.querySelectorAll('#categories .item');
+// console.log(`Number of categories: ${categoriesList.length}`);
 
-categoriesList.forEach(category => {
-  const title = category.querySelector('h2').textContent;
-  const itemsCount = category.querySelectorAll('ul li').length;
+// categoriesList.forEach(category => {
+//   const title = category.querySelector('h2').textContent;
+//   const itemsCount = category.querySelectorAll('ul li').length;
 
-  console.log(`Category: ${title}`);
-  console.log(`Elements: ${itemsCount}`);
-});
+//   console.log(`Category: ${title}`);
+//   console.log(`Elements: ${itemsCount}`);
+// });
--- js/task-4.js
+++ js/task-4.js
@@ -1,22 +1,22 @@
-const form = document.querySelector('.login-form');
+// const form = document.querySelector('.login-form');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
 
-  const { email, password } = event.currentTarget.elements;
+//   const { email, password } = event.currentTarget.elements;
 
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
+//   const emailValue = email.value.trim();
+//   const passwordValue = password.value.trim();
 
-  if (!emailValue || !passwordValue) {
-    return alert('All form fields must be filled in');
-  }
+//   if (!emailValue || !passwordValue) {
+//     return alert('All form fields must be filled in');
+//   }
 
-  const formData = {
-    email: emailValue,
-    password: passwordValue,
-  };
+//   const formData = {
+//     email: emailValue,
+//     password: passwordValue,
+//   };
 
-  console.log(formData);
-  form.reset();
-});
+//   console.log(formData);
+//   form.reset();
+// });
