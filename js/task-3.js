fe987a7d71caac17a5859f10b2d3f598bd7e56f1
Manokha Vadim <manokha.vadim@gmail.com>
2025-04-22 23:11:10 +0300

    modified project

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-// const inputEl = document.querySelector('#name-input');
-// const outputEl = document.querySelector('#name-output');
-
-// inputEl.addEventListener('input', event => {
-//   const trimmed = event.target.value.trim();
-//   outputEl.textContent = trimmed ? trimmed : 'Anonymous';
-// });
